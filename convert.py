a9895f5c4d68ef3d3f7506be7413ee37b06799f5
joris <jorisvddonk@gmail.com>
2020-03-18 18:11:29 +0100

    initial commit

---
+++ convert.py
@@ -0,0 +1,43 @@
+import os
+import json
+from markdown import Markdown
+from io import StringIO
+
+
+def unmark_element(element, stream=None):
+    if stream is None:
+        stream = StringIO()
+    if element.text:
+        stream.write(element.text)
+    for sub in element:
+        unmark_element(sub, stream)
+    if element.tail:
+        stream.write(element.tail)
+    return stream.getvalue()
+
+
+# patching Markdown
+Markdown.output_formats["plain"] = unmark_element
+__md = Markdown(output_format="plain")
+__md.stripTopLevelTags = False
+
+
+def unmark(text):
+    return __md.convert(text)
+
+
+out = open('./data.txt', 'w', encoding="utf8")
+num = 0
+
+for f in os.listdir("./data"):
+    fil = open(os.path.join("./data", f), 'r', encoding="utf8")
+    for line in fil:
+        data = json.loads(line)
+        entry = "<REQUEST>\n%s\n%s\n\n<REPLY>\n%s\n\n\n" % (
+            data['title'], unmark(data['selftext']), unmark(data['body']))
+        out.write(entry)
+        num += 1
+    fil.close()
+
+print(num)
+out.close()
